83eb5fe9ea6fc77ea005894a80038bab7e782a91
michele-pupo <mikypupo@hotmail.it>
2024-02-29 14:31:11 +0100

    end navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script>
+    import AppNav from './components/AppNav.vue';
 
+    export default{
+
+    
+    components: {
+        AppNav,
+    },
+
+}
 </script>
 
 <template>
-  
+    
+    <AppNav></AppNav>
+
 </template>
 
-<style>
-
+<style lang="scss">
+  @use './styles/general.scss' as *;
 </style>
---
+++ src/components/AppNav.vue
@@ -0,0 +1,64 @@
+<script>
+    export default{
+        name: 'AppNav',
+
+        data() {
+            return {
+                links: [
+                        "Characters",
+                        "Comics",
+                        "Movies",
+                        "Tv",
+                        "Games",
+                        "Collectibles",
+                        "Videos",
+                        "fans",
+                        "News",
+                        "Shop"
+                        ]
+            }
+        },
+    }
+</script>
+
+<template>
+    <div id="navbar">
+        <div id="container">
+            <img src="/img/dc-logo.png" alt="">
+            <ul>
+                <li v-for="currentLink in links">
+                    <a href="">{{currentLink}}</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+  
+</template>
+
+<style>
+    #navbar{
+        background-color: white;
+        padding: 20px 0px;
+
+        #container{
+            width: 1250px;
+            margin: 0 auto;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+
+            ul{
+                display: flex;
+                gap: 35px;
+
+                a{
+                    text-decoration: none;
+                    text-transform: uppercase;
+                    font-weight: bold;
+                    color: black;
+                }
+            }
+        }
+    }
+
+</style>
