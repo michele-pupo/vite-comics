e83be241632a081904d5b110c640f429630263bb
michele-pupo <mikypupo@hotmail.it>
2024-03-01 14:36:36 +0100

    inserted comics dynamically

--- src/components/AppBooks.vue
+++ src/components/AppBooks.vue
@@ -1,6 +1,89 @@
 <script>
+    import ComicItem from './ComicItem.vue'
+
     export default{
         name: 'AppBooks',
+
+        components:{ComicItem},
+
+        data() {
+            return {
+                comicBook:[
+                        {
+                            "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                            "price": "$19.99",
+                            "series": "Action Comics",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                            "price": "$3.99",
+                            "series": "American Vampire 1976",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                            "price": "$16.99",
+                            "series": "Aquaman",
+                            "type": "graphic novel",
+                        },
+                        {
+                            "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                            "price": "$2.99",
+                            "series": "Batgirl",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                            "price": "$3.99",
+                            "series": "Batman",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                            "price": "$2.99",
+                            "series": "Batman Beyond",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                            "price": "$3.99",
+                            "series": "Batman/Superman",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                            "price": "$4.99",
+                            "series": "Batman/Superman Annual",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                            "price": "$5.99",
+                            "series": "Batman: The Joker War Zone",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                            "price": "$6.99",
+                            "series": "Batman: Three Jokers",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                            "price": "$4.99",
+                            "series": "Batman: White Knight Presents: Harley Quinn",
+                            "type": "comic book",
+                        },
+                        {
+                            "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                            "price": "$16.99",
+                            "series": "Catwoman",
+                            "type": "graphic novel",
+                        },
+                ]
+            }
+        },
     }
 </script>
 
@@ -8,9 +91,16 @@
     <div id="books-comic">
         <div id="container-books">
             <button>Current series</button>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit non ut amet corporis nam delectus id consequatur explicabo accusantium obcaecati, alias cupiditate quam consectetur unde in nostrum laboriosam maiores quae!</p>
+            <div id="comics">
+                <div id="comics-list">
+                    <ComicItem 
+                        v-for="currentComic in comicBook"
+                        :comicName="currentComic.series"
+                        :comicImage="currentComic.thumb"
+                    ></ComicItem>
+                </div>
+            </div>
         </div>
-        
     </div>
 </template>
 
@@ -18,25 +108,35 @@
     @use '../styles/variables' as *;
 
     #books-comic{
-        height: 670px;
+        height: 750px;
         width: 100%;
         background-color: #1c1c1c;
-
-        button{
-            position: relative;
-            top: -25px;
-            left: -20px;
-            background-color: $primaryColorBlue;
-            padding: 10px 30px;
-            border: transparent;
-            font-size: 25px;
-            text-transform: uppercase;
-            font-weight: bold;
-        }
 
         #container-books{
             width: 1250px;
             margin: 0 auto;
+
+            button{
+                position: relative;
+                top: -25px;
+                left: -20px;
+                background-color: $primaryColorBlue;
+                padding: 10px 30px;
+                border: transparent;
+                font-size: 25px;
+                text-transform: uppercase;
+                font-weight: bold;
+            }
+                
+            #comics{
+                    
+                #comics-list{
+                    display: flex;
+                    flex-flow: row wrap;
+                    gap: 30px;
+                }
+            }
+            
         }
     }
 </style>
---
+++ src/components/ComicItem.vue
@@ -0,0 +1,37 @@
+<script>
+    export default{
+        name: 'ComicItem',
+
+        props:{
+            comicName: String,
+            comicImage: String
+        }
+    }
+</script>
+
+<template>
+
+   <div id="single-comic">
+        <img :src="comicImage" alt="">
+        <h4>{{ comicName }}</h4>
+    </div>
+   
+</template>
+
+<style lang="scss">
+    @use '../styles/variables' as *;
+
+    #single-comic{
+        width: calc(100% / 6 - 30px / 6 * 5);
+
+        img{
+            width: 100%;
+            height: 250px;
+        }
+    }
+   
+</style>
+
+
+
+
